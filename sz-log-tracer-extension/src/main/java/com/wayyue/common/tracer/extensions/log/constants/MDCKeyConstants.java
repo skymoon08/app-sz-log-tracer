c99ea423dbdc916e0168d4ea1656500afcba7730
肖金明 <xiaojm@wayyue.com>
2020-06-02 19:55:12 +0800

    update file

--- sz-log-tracer-extension/src/main/java/com/wayyue/common/tracer/extensions/log/constants/MDCKeyConstants.java
+++ sz-log-tracer-extension/src/main/java/com/wayyue/common/tracer/extensions/log/constants/MDCKeyConstants.java
@@ -5,7 +5,6 @@ package com.wayyue.common.tracer.extensions.log.constants;
  * Responsible for managing key value constants that need to be stored in MDC
  * @author jinming.xiao
  * @since 2020/06/01
- * @version $Id: MDCKeyConstants.java, v 0.1 August 07, 2017 8:44 AM luoguimu123 Exp $
  */
 public class MDCKeyConstants {
 
